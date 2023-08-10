2a73604906140ead1541fb4462ab86573d617f48
mohammadaminmg10 <ghorbannezhadm2@gmail.com>
2023-08-10 16:07:14 +0330

    change database

--- main.go
+++ main.go
@@ -11,7 +11,9 @@ import (
 )
 
 func setupDB() (*sql.DB, error) {
-	db, err := sql.Open("postgres", "dbname=AnonChat user=moamin password=mg1383 host=localhost port=5432 sslmode=disable")
+	connStr := "postgres://moamin:1TLkY3LVPsffik92zVZf33eiXv2wacMM@dpg-cjad7f6e546c738c72o0-a.oregon-postgres.render.com/anonchat"
+	db, err := sql.Open("postgres", connStr)
+	//	db, err := sql.Open("postgres", "dbname=anonchat user=moamin password=1TLkY3LVPsffik92zVZf33eiXv2wacMM host=dpg-cjad7f6e546c738c72o0-a port=5432 sslmode=disable")
 	if err != nil {
 		return nil, err
 	}
@@ -24,6 +26,36 @@ func main() {
 		log.Fatal(err)
 	}
 	defer DB.Close()
+	_, err = DB.Exec(`
+       CREATE SCHEMA anonchat;
+    `)
+	// Create the "messages" table
+	_, err = DB.Exec(`
+        CREATE TABLE IF NOT EXISTS AnonChat.messages (
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(50) NOT NULL,
+            nickname VARCHAR(50),
+            text TEXT NOT NULL,
+            timestamp TIMESTAMPTZ NOT NULL
+        );
+    `)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Messages table created successfully.")
+
+	// Create the "users" table
+	_, err = DB.Exec(`
+        CREATE TABLE IF NOT EXISTS AnonChat.users (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(100) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL
+        );
+    `)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Users table created successfully.")
 	// Assuming your static files are in the "static" directory
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
